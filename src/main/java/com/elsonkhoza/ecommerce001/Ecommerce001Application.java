94c600fdbbc294573ac8ad8444fa6a0a04019500
elsoncom <145992011+elsoncom@users.noreply.github.com>
2023-10-24 17:38:36 +0200

    Project Setup

---
+++ src/main/java/com/elsonkhoza/ecommerce001/Ecommerce001Application.java
@@ -0,0 +1,13 @@
+package com.elsonkhoza.ecommerce001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ecommerce001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ecommerce001Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/elsonkhoza/ecommerce001/Ecommerce001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elsonkhoza.ecommerce001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ecommerce001ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
